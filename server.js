83020c066d661a4e49aba1a179c18ce4dee12bf4
Sanika Patil <13sanikapatil@gmail.com>
2025-12-27 22:47:59 +0530

    Increase Express body limit to 50MB for Base64 resume storage

--- server.js
+++ server.js
@@ -25,8 +25,8 @@ connectDB();
 
 // Middleware
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use("/uploads", express.static("uploads"));
 
 // Routes
